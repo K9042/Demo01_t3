aaf8a2eca8534dc7e4805bfd03130721656bedb5
K9042 <K9042@student.jamk.fi>
2017-01-19 14:52:13 +0200

    Done

--- teh3/Program.cs
+++ teh3/Program.cs
@@ -10,6 +10,22 @@ namespace teh3
     {
         static void Main(string[] args)
         {
+            int luku1, luku2, luku3, summa, keskiarvo;
+
+            Console.Write("Anna luku1 > ");
+            luku1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna luku2 > ");
+            luku2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Anna luku3 > ");
+            luku3 = int.Parse(Console.ReadLine());
+
+            summa = luku1 + luku2 + luku3;
+            keskiarvo = summa / 3;
+
+            Console.WriteLine("Lukujen summa: " + summa);
+            Console.WriteLine("Lukujen keskiarvo: " + keskiarvo);
         }
     }
 }
